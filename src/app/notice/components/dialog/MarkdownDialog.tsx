03f2fa6ea8273b05dd7618a66eade80cffa7e245
pc <svx327@naver.com>
2025-04-03 03:41:04 +0900

    Add: LabelCalendar 컴포넌트에 날짜 선택 기능 추가 및 MarkdownDialog에서 날짜 선택 로직 통합

--- src/app/notice/components/dialog/MarkdownDialog.tsx
+++ src/app/notice/components/dialog/MarkdownDialog.tsx
@@ -10,7 +10,6 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -18,58 +17,77 @@ import {
 } from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
-import LabelCalendar from "../calendar/LabelCalendar";
 import { toast } from "sonner";
-import { createTodo } from "@/app/actions/todos-action";
+import LabelCalendar from "../calendar/LabelCalendar";
 
-function MarkdownDialog() {
+// contents 배열에 대한 타입 정의
+interface BoardContent {
+  isCompleted: boolean;
+  title: string;
+  content: string;
+  startDate: string | Date;
+  endDate: string | Date;
+  boardId: string; // 랜던함 아이디를 생성해줄 예정
+}
+
+interface BasicBoardProps {
+  item: BoardContent;
+  updateContent: (newData: BoardContent) => void;
+}
+
+function MarkdownDialog({ item, updateContent }: BasicBoardProps) {
   // 다이얼로그 Props
   const [open, setOpen] = useState<boolean>(false);
 
   // 에디터의 제목/본문 내용
-  const [title, setTitle] = useState<string | undefined>("");
-  const [contents, setContents] = useState<string | undefined>("");
+  const [title, setTitle] = useState<string | undefined>(
+    item.title ? item.title : ""
+  );
+  const [content, setContent] = useState<string | undefined>(
+    item.content ? item.content : ""
+  );
+
+  const [startDate, setStartDate] = useState<Date | undefined>(new Date());
+  const [endDate, setEndDate] = useState<Date | undefined>(new Date());
+
+  const [isCompleted, setIsComplted] = useState<boolean>(
+    item.isCompleted ? item.isCompleted : false
+  );
 
   // todo 작성
   const onSubmit = async () => {
-    if (!title || !contents) {
+    if (!title || !content || !startDate || !endDate) {
       toast.error("입력항목을 확인해 주세요.", {
-        description: "제목과 내용을 입력해주세요.",
+        description: "제목, 내용, 날짜를 입력해주세요.",
         duration: 3000,
       });
       return;
     }
 
-    // 서버액션 실행하기
-    const { data, error, status } = await createTodo({
-      contents: contents,
+    // 해당 Row 를 바로 업데이트 하는 것이 아니고,
+    // contents 칼럼의 [] 을 업데이트하고.. 실제 Row 를 업데이트 해야함.
+    const tempContent: BoardContent = {
+      boardId: item.boardId,
+      startDate: startDate,
+      endDate: endDate,
       title: title,
-    });
-
-    if (error) {
-      toast.error("등록 실패.", {
-        description: `Error ${error.message}`,
-        duration: 3000,
-      });
-      return;
-    }
-
-    toast.success("성공하였습니다.", {
-      description: "Supabase에 글이 등록되었습니다.",
-      duration: 3000,
-    });
+      content: content,
+      isCompleted: isCompleted,
+    };
+    updateContent(tempContent);
 
     // 창닫기
     setOpen(false);
-    setTitle("");
-    setContents("");
+    // setTitle("");
+    // setContent("");
   };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <span className="w-full justify-center flex font-normal text-gray-400 hover:text-gray-500 cursor-pointer">
-          Add Content
+          {/* 현재 내용이 있는 경우와 내용이 없는 경우로 구분 */}
+          {item.title ? "Modify Content" : "Add Content"}
         </span>
       </DialogTrigger>
       <DialogContent className="max-w-fit min-w-[600px]">
@@ -86,15 +104,25 @@ function MarkdownDialog() {
               />
             </div>
           </DialogTitle>
-          <DialogDescription />
           <div className={styles.dialog_calendarBox}>
-            <LabelCalendar label="From" required={false} />
-            <LabelCalendar label="To" required={false} />
+            {/* 잠시 뒤 날짜 전달 */}
+            <LabelCalendar
+              label="From"
+              required={false}
+              selectedDate={startDate}
+              onDateChange={setStartDate}
+            />
+            <LabelCalendar
+              label="To"
+              required={false}
+              selectedDate={endDate}
+              onDateChange={setEndDate}
+            />
           </div>
           <Separator />
           {/* 마크다운 입력 영역 */}
           <div className={styles.dialog_markdown}>
-            <MDEditor height={"100%"} value={contents} onChange={setContents} />
+            <MDEditor height={"100%"} value={content} onChange={setContent} />
           </div>
         </DialogHeader>
         <DialogFooter>
